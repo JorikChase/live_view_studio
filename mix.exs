efbd5f97fd4d1caf1babe63e38c107b15685b5c6
Jorik Chase <jorikchase@gmail.com>
2023-07-23 03:59:05 +0200

    swag

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule LiveViewStudio.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
+      {:distillery, "~> 2.1"},
       {:plug_cowboy, "~> 2.5"}
     ]
   end
